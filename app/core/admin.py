21167138642f66476c59af3fda5f5d23021e5b02
Gustavo Sooeiro <gustavosooeiro@gmail.com>
2020-12-02 15:52:38 -0300

    List ingredients feature

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
-from core.models import User, Tag
+from core.models import User, Tag, Ingredient
 
 
 @admin.register(User)
@@ -27,4 +27,9 @@ class UserAdmin(BaseUserAdmin):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
     pass
